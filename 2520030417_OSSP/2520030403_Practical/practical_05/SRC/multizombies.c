3b4f186a4711192be77b041fe2047bcb36667af3
PavanSai <pavansaip.ps2@gmail.com>
2026-09-10 21:19:11 +0530

    Add files via upload

---
+++ 2520030417_OSSP/2520030403_Practical/practical_05/SRC/multizombies.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+int main()
+{
+int pid[3];
+for(int i = 0; i < 3; i++)
+{
+pid[i] = fork();
+if(pid[i] == 0)
+{
+printf("Child %d PID = %d\n", i + 1, getpid());
+sleep(70);
+exit(0);
+}
+}
+wait(NULL);
+printf("Parent: One child completed using wait()\n");
+waitpid(pid[2], NULL, 0);
+printf("Parent: Child 3 completed using waitpid()\n");
+wait(NULL);
+printf("Parent: All children completed\n");
+}
---
+++ 2520030417_OSSP/2520030403_Practical/practical_05/SRC/prodcons.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    int pipefd[2], data = 100;
+    pipe(pipefd);
+
+    if (fork() == 0) {   // Child: Consumer
+        close(pipefd[1]);
+        read(pipefd[0], &data, sizeof(data));
+        printf("Child consumed: %d\n", data);
+        close(pipefd[0]);
+    } else {     // Parent: Producer
+        close(pipefd[0]);
+        printf("Parent produced: %d\n", data);
+        write(pipefd[1], &data, sizeof(data));
+        close(pipefd[1]);
+        wait(NULL);
+    }
+
+    return 0;
+}
